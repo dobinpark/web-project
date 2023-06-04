8e40c580f9bc5eb5ee86bf7791751c9b045a4241
DOBIN <devpark0810@gmail.com>
2023-06-04 17:09:23 +0900

    2023.06.04 홈페이지 뷰 수정

--- src/main/java/dobin/webproject/naver/wishlist/repository/WishListRepository.java
+++
@@ -1,9 +0,0 @@
-package dobin.webproject.naver.wishlist.repository;
-
-import dobin.webproject.naver.db.MemoryDbRepositoryAbstract;
-import dobin.webproject.naver.wishlist.entity.WishListEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository // DB를 저장하는 곳이다라고 지정
-public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
-}
